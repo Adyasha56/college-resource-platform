7a5bd13c8e97f84308d713b85dba3aaa32d60cab
Adyasha Nanda <adyashananda105@gmail.com>
2025-07-03 22:36:25 +0530

    added the register page

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 // Pages (yeh sab bana lenge one-by-one)
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-// import Register from "../pages/Register";
+import Register from "../pages/Register";
 // import QuestionPapers from "../pages/QuestionPapers";
 // import Placements from "../pages/Placements";
 // import AdminDashboard from "../pages/admin/Dashboard";
@@ -14,8 +14,8 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/register" element={<Register />} />
-      <Route path="/question-papers" element={<QuestionPapers />} />
+       <Route path="/register" element={<Register />} />
+      {/*<Route path="/question-papers" element={<QuestionPapers />} />
       <Route path="/placements" element={<Placements />} />
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="*" element={<NotFound />} />  */}
