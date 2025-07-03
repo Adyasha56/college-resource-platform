65f4c52362a35be943cd5aa9450af40c62b662bd
Adyasha Nanda <adyashananda105@gmail.com>
2025-07-03 21:44:54 +0530

    added login page and fix the cors err

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 function App() {
   return (
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages (yeh sab bana lenge one-by-one)
 import Home from "../pages/Home";
-// import Login from "../pages/Login";
+import Login from "../pages/Login";
 // import Register from "../pages/Register";
 // import QuestionPapers from "../pages/QuestionPapers";
 // import Placements from "../pages/Placements";
@@ -13,12 +13,12 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/register" element={<Register />} />
       <Route path="/question-papers" element={<QuestionPapers />} />
       <Route path="/placements" element={<Placements />} />
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />  */}
     </Routes>
   );
 };
