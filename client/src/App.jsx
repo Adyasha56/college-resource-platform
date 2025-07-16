920a74f86a013a0cab14238ce2202f733bab50b2
Adyasha Nanda <adyashananda105@gmail.com>
2025-07-16 15:37:35 +0530

    fix the dual navbar issue and login issues in admin side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-// src/App.jsx
+import { useLocation } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  const location = useLocation();
+
+  // Hide Navbar and Footer on admin pages
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
   return (
     <div className="bg-yellow-100 min-h-screen flex flex-col">
-      <Navbar />
-      
+      {!isAdminRoute && <Navbar />}
       
       <main className="flex-grow">
         <AppRoutes />
       </main>
-
-      <Footer />
+      
+      {!isAdminRoute && <Footer />}
     </div>
   );
 }
